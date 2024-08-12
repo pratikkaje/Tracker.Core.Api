ad925fec226bc86404974c9cf809a95b2966a1a5
Pratik Kaje <kajepratik@gmail.com>
2024-08-12 21:47:07 +0400

    INFRA: Initialize Api project

---
+++ Tracker.Core.Api/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tracker.Core.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok("Hello World!");
+    }
+}
