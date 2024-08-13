1d9da099dcf34fcbc177b5625aeaf40585b68fd4
Pratik Kaje <kajepratik@gmail.com>
2024-08-13 19:56:50 +0400

    INFRA: Build Script

---
+++ Tracker.Core.Infrastructure/Program.cs
@@ -0,0 +1,17 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Tracker.Core.Infrastructure.Services;
+
+namespace Tracker.Core.Infrastructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var scriptGenerationService = new ScriptGenerationService();
+            scriptGenerationService.GenerateBuildScript();
+        }
+    }
+}
