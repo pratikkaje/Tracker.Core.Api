4d1da9b2a8f26bcecb471b2622a4e569958ba7c8
Pratik Kaje <kajepratik@gmail.com>
2024-08-14 17:56:06 +0400

    INFRA: Initialize Unit Tests.

---
+++ Tracker.Core.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,13 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+using Xunit;
+
+namespace Tracker.Core.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(condition: true);
+    }
+}
