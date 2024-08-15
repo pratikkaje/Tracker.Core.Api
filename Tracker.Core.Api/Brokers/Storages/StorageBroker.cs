42905114e332914b95426748284aceaa1a888111
Pratik Kaje <kajepratik@gmail.com>
2024-08-15 19:54:36 +0400

    BROKERS: Initialize Storage Broker

---
+++ Tracker.Core.Api/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,26 @@
+﻿using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Tracker.Core.Api.Brokers.Storages
+{
+    internal class StorageBroker : EFxceptionsContext, IStorageBroker
+    {
+        private readonly IConfiguration configuration;
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.Database.Migrate();
+        }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString =
+                this.configuration.GetConnectionString(
+                    name: "DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
