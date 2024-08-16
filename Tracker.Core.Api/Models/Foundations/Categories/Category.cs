9ab95b4df55010eb2fd48d5d5dd1d43de620ea26
Pratik Kaje <kajepratik@gmail.com>
2024-08-16 14:10:54 +0400

    DATA: Add Categories Table Constraints

--- Tracker.Core.Api/Models/Foundations/Categories/Category.cs
+++ Tracker.Core.Api/Models/Foundations/Categories/Category.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Tracker.Core.Api.Models.Foundations.Categories
 {
+
+    [Index(nameof(Name), IsUnique = true)]
     public class Category
     {
         public Guid Id { get; set; }
+        [Required]
+        [MaxLength(255)]
         public string Name { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime UpdatedDate { get; set; }
