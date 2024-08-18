37d09957f5ef38657697f9b3843f04e5ae2f6b44
Pratik Kaje <kajepratik@gmail.com>
2024-08-18 15:17:56 +0400

    Foundations: Add Category Service

---
+++ Tracker.Core.Api/Services/Foundations/Categories/CategoryService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Tracker.Core.Api.Brokers.Loggings;
+using Tracker.Core.Api.Brokers.Storages;
+using Tracker.Core.Api.Models.Foundations.Categories;
+
+namespace Tracker.Core.Api.Services.Foundations.Categories
+{
+    internal class CategoryService : ICategoryService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public CategoryService(
+            IStorageBroker storageBroker, 
+            ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<Category> AddCategoryAsync(Category category)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
