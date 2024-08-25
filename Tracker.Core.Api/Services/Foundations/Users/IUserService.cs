4d41dcf64cc060c051b02b30ed0bf167cc35059b
Pratik Kaje <kajepratik@gmail.com>
2024-08-25 13:15:42 +0400

    FOUNDATIONS: Add User interface and service

---
+++ Tracker.Core.Api/Services/Foundations/Users/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Tracker.Core.Api.Models.Foundations.Users;
+
+namespace Tracker.Core.Api.Services.Foundations.Users
+{
+    public interface IUserService
+    {
+        ValueTask<User> AddCategoryAsync(User user);
+    }
+}
---
+++ Tracker.Core.Api/Services/Foundations/Users/UserService.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Tracker.Core.Api.Brokers.Loggings;
+using Tracker.Core.Api.Brokers.Storages;
+using Tracker.Core.Api.Models.Foundations.Users;
+
+namespace Tracker.Core.Api.Services.Foundations.Users
+{
+    internal class UserService : IUserService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public UserService(IStorageBroker storageBroker, ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<User> AddCategoryAsync(User user)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
