f40255787db368206d9b8fc6f52c48dd06819b32
Pratik Kaje <kajepratik@gmail.com>
2024-09-06 23:16:11 +0400

    CODE RUB: Moved model contrainsts to its own configuration file under brokers.

---
+++ Tracker.Core.Api/Brokers/Storages/StorageBroker.Users.Configuration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Tracker.Core.Api.Models.Foundations.Users;
+
+namespace Tracker.Core.Api.Brokers.Storages
+{
+    internal partial class StorageBroker
+    {
+        private void AddUserConfigurations(EntityTypeBuilder<User> builder)
+        {
+            builder
+                .Property(user => user.UserName)
+                .IsRequired()
+                .HasMaxLength(300);
+
+            builder.Property(user => user.Name)
+                .HasMaxLength(400);
+
+            builder.Property(user => user.Email)
+                .IsRequired()
+                .HasMaxLength(400);
+        }
+    }
+}
