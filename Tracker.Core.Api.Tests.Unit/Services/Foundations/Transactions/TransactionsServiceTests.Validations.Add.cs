97a4171634d548820afb2993171f7e70e2c71b3c
Pratik Kaje <kajepratik@gmail.com>
2024-09-24 22:28:48 +0400

    ShouldThrowValidationExceptionOnAddIfTransactionIsInvalidAndLogItAsync => FAIL

--- Tracker.Core.Api.Tests.Unit/Services/Foundations/Transactions/TransactionsServiceTests.Validations.Add.cs
+++ Tracker.Core.Api.Tests.Unit/Services/Foundations/Transactions/TransactionsServiceTests.Validations.Add.cs
@@ -51,5 +51,109 @@ namespace Tracker.Core.Api.Tests.Unit.Services.Foundations.Transactions
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.datetimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfTransactionIsInvalidAndLogItAsync(string invalidString)
+        {
+            // given
+            DateTimeOffset randomDateTimeOffset = default;
+
+            Transaction invalidTransaction = new Transaction
+            {
+                Id = Guid.Empty,
+                UserId = Guid.Empty,
+                CategoryId = Guid.Empty,
+                TransactionType = invalidString,
+                Amount = default,
+                Description = invalidString,
+                TransactionDate = randomDateTimeOffset,
+                CreatedBy = invalidString,
+                UpdatedBy = invalidString,
+                CreatedDate = randomDateTimeOffset,
+                UpdatedDate = randomDateTimeOffset,
+            };
+
+            var invalidTransactionException = 
+                new InvalidTransactionException(
+                    message: "Transaction is invalid, fix the errors and try again.");
+
+            invalidTransactionException.AddData(
+                key: nameof(Transaction.Id), 
+                values: "Id is invalid.");
+
+            invalidTransactionException.AddData(
+                key: nameof(Transaction.UserId),
+                values: "Id is invalid.");
+
+            invalidTransactionException.AddData(
+                key: nameof(Transaction.CategoryId),
+                values: "Id is invalid.");
+
+            invalidTransactionException.AddData(
+                key: nameof(Transaction.TransactionType),
+                values: "Text is required.");
+
+            invalidTransactionException.AddData(
+                key: nameof(Transaction.Amount),
+                values: "Value greater than 0 is required.");
+
+            invalidTransactionException.AddData(
+                key: nameof(Transaction.Description),
+                values: "Text is required.");
+
+            invalidTransactionException.AddData(
+                key: nameof(Transaction.TransactionDate), 
+                values: "Date is invalid.");
+
+            invalidTransactionException.AddData(
+                key: nameof(Transaction.CreatedBy),
+                values: "Text is required.");
+
+            invalidTransactionException.AddData(
+                key: nameof(Transaction.UpdatedBy),
+                values: "Text is required.");
+
+            invalidTransactionException.AddData(
+                key: nameof(Transaction.CreatedDate),
+                values: "Date is invalid.");
+
+            invalidTransactionException.AddData(
+                key: nameof(Transaction.UpdatedDate),
+                values: "Date is invalid.");
+
+            TransactionValidationException expectedTransactionValidationException = 
+                new TransactionValidationException(
+                    message: "Transaction validation error occurred, fix the errors and try again.", 
+                    innerException: invalidTransactionException);
+
+            // when
+            ValueTask<Transaction> addTransactionTaskAsync = 
+                this.transactionService.AddTransactionAsync(invalidTransaction);
+
+            TransactionValidationException actualTransactionValidationException = 
+                await Assert.ThrowsAsync<TransactionValidationException>(addTransactionTaskAsync.AsTask);
+
+            // then
+            actualTransactionValidationException.Should().BeEquivalentTo(
+                expectedTransactionValidationException);
+
+            this.loggingBrokerMock.Verify(broker => 
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedTransactionValidationException))), 
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker => 
+                broker.InsertTransactionAsync(
+                    It.IsAny<Transaction>()), 
+                        Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.datetimeBrokerMock.VerifyNoOtherCalls();
+        }
+
     }
 }
--- Tracker.Core.Api/Models/Foundations/Transactions/Transaction.cs
+++ Tracker.Core.Api/Models/Foundations/Transactions/Transaction.cs
@@ -16,6 +16,8 @@ namespace Tracker.Core.Api.Models.Foundations.Transactions
         public decimal Amount { get; set; }
         public string Description { get; set; }
         public DateTimeOffset TransactionDate { get; set; }
+        public string CreatedBy {  get; set; }
+        public string UpdatedBy { get; set; }
         public DateTimeOffset CreatedDate { get; set; }
         public DateTimeOffset UpdatedDate { get; set; }
         public User User { get; set; }
