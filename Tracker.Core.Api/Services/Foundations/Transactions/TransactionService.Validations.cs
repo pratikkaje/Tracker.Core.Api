9512266e2ab424c20143733b2a3880e7cb121f1b
Pratik Kaje <kajepratik@gmail.com>
2024-10-02 20:45:46 +0400

    ShouldThrowValidationExceptionOnAddIfCreatedDateIsNotRecentAndLogItAsync => PASS

--- Tracker.Core.Api/Services/Foundations/Transactions/TransactionService.Validations.cs
+++ Tracker.Core.Api/Services/Foundations/Transactions/TransactionService.Validations.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data;
+using System.Reflection.Metadata;
 using System.Threading.Tasks;
 using Tracker.Core.Api.Models.Foundations.Transactions;
 using Tracker.Core.Api.Models.Foundations.Transactions.Exceptions;
@@ -48,7 +50,43 @@ namespace Tracker.Core.Api.Services.Foundations.Transactions
                 (Rule: await IsNotSameAsync(
                     firstDate: transaction.UpdatedDate,
                     secondDate: transaction.CreatedDate,
-                    secondDateName: nameof(Transaction.CreatedDate)), Parameter: nameof(Transaction.UpdatedDate)));
+                    secondDateName: nameof(Transaction.CreatedDate)), Parameter: nameof(Transaction.UpdatedDate)),
+
+                (Rule: await IsNotRecentAsync(transaction.CreatedDate), Parameter: nameof(Transaction.CreatedDate)));
+        }
+
+        private async ValueTask<dynamic> IsNotRecentAsync(DateTimeOffset date)
+        {
+            var (isNotRecent, startDate, endDate) = await IsDateNotRecentAsync(date);
+
+            return new
+            {
+                Condition = isNotRecent,
+                Message = $"Date is not recent. Expected a value between {startDate} and {endDate} but found {date}"
+            };
+
+        }
+
+        private async ValueTask<(bool IsNotRecent, DateTimeOffset StartDate, DateTimeOffset EndDate)>
+            IsDateNotRecentAsync(DateTimeOffset date)
+        {
+            int pastSeconds = 60;
+            int futureSeconds = 0;
+
+            DateTimeOffset currentDateTime =
+                await this.dateTimeBroker.GetCurrentDateTimeOffsetAsync();
+
+            if (currentDateTime == default)
+            {
+                return (false, default, default);
+            }
+
+            TimeSpan timeDifference = currentDateTime.Subtract(date);
+            DateTimeOffset startDate = currentDateTime.AddSeconds(-pastSeconds);
+            DateTimeOffset endDate = currentDateTime.AddSeconds(futureSeconds);
+            bool isNotRecent = timeDifference.TotalSeconds is > 60 or < 0;
+
+            return (isNotRecent, startDate, endDate);
         }
 
         private static async ValueTask<dynamic> IsNotSameAsync(
