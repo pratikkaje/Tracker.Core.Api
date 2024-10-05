cf76ea58fd88ba7269509fc6e7b9bc2116f4a7db
Pratik Kaje <kajepratik@gmail.com>
2024-10-05 22:32:54 +0400

    ShouldThrowDependencyValidationExceptionOnAddIfTransactionAlreadyExistsAndLogItAsync => FAIL

--- Tracker.Core.Api.Tests.Unit/Services/Foundations/Transactions/TransactionsServiceTests.Exceptions.Add.cs
+++ Tracker.Core.Api.Tests.Unit/Services/Foundations/Transactions/TransactionsServiceTests.Exceptions.Add.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
 using Moq;
 using Tracker.Core.Api.Models.Foundations.Transactions;
 using Tracker.Core.Api.Models.Foundations.Transactions.Exceptions;
@@ -48,6 +51,56 @@ namespace Tracker.Core.Api.Tests.Unit.Services.Foundations.Transactions
             this.loggingBrokerMock.Verify(broker => 
                 broker.LogCriticalAsync(It.Is(SameExceptionAs(
                     expectedTransactionDependencyException))), 
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker => 
+                broker.InsertTransactionAsync(
+                    It.IsAny<Transaction>()), 
+                        Times.Never);
+
+            this.datetimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationExceptionOnAddIfTransactionAlreadyExistsAndLogItAsync()
+        {
+            // given
+            Transaction someTransaction = CreateRandomTransaction();
+
+            var duplicateKeyException =
+                new DuplicateKeyException(
+                    message: "Transaction already exists error occurred.");
+
+            var expectedTransactionDependencyValidationException =
+                new TransactionDependencyValidationException(
+                    message: "Transaction dependency validation error occurred. Please fix errors and try again.",
+                    innerException: duplicateKeyException);
+
+            this.datetimeBrokerMock.Setup(broker => 
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ThrowsAsync(duplicateKeyException);
+
+            // when
+            ValueTask<Transaction> addTransactionTask =
+                this.transactionService.AddTransactionAsync(someTransaction);
+
+            TransactionDependencyValidationException actualTransactionDependencyValidationException =
+                await Assert.ThrowsAsync<TransactionDependencyValidationException>(
+                    addTransactionTask.AsTask);
+
+            // then
+            actualTransactionDependencyValidationException.Should().BeEquivalentTo(
+                expectedTransactionDependencyValidationException);
+
+            this.datetimeBrokerMock.Verify(broker => 
+                broker.GetCurrentDateTimeOffsetAsync(), 
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker => 
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedTransactionDependencyValidationException))), 
                         Times.Once);
 
             this.storageBrokerMock.Verify(broker => 
