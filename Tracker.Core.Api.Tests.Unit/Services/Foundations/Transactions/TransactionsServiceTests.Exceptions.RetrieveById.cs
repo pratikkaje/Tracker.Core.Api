ce16190d78ad6d8afbfc6034e972f7701a1d0388
Pratik Kaje <kajepratik@gmail.com>
2024-10-20 16:39:41 +0400

    ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync => FAIL

---
+++ Tracker.Core.Api.Tests.Unit/Services/Foundations/Transactions/TransactionsServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Tracker.Core.Api.Models.Foundations.Transactions;
+using Tracker.Core.Api.Models.Foundations.Transactions.Exceptions;
+
+namespace Tracker.Core.Api.Tests.Unit.Services.Foundations.Transactions
+{
+    public partial class TransactionsServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            var someTransactionId = Guid.NewGuid();
+            var sqlException = CreateSqlException();
+
+            var failedStorageTransactionException =
+                new FailedStorageTransactionException(
+                    message: "Failed transaction storage error occurred, contact support.",
+                    innerException: sqlException);
+
+            var expectedTransactionDependencyException =
+                new TransactionDependencyException(
+                    message: "Transaction dependency error occurred, contact support.",
+                    innerException: failedStorageTransactionException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectTransactionByIdAsync(someTransactionId))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<Transaction> retrieveTransactionByIdTask =
+                this.transactionService.RetrieveTransactionByIdAsync(someTransactionId);
+
+            TransactionDependencyException actualTransactionDependencyException =
+                await Assert.ThrowsAsync<TransactionDependencyException>(
+                    testCode: retrieveTransactionByIdTask.AsTask);
+
+            // then
+            actualTransactionDependencyException.Should().BeEquivalentTo(
+                expectedTransactionDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectTransactionByIdAsync(someTransactionId),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedTransactionDependencyException))),
+                        Times.Once);
+
+            this.datetimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.datetimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
