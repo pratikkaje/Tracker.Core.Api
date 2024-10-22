3e6e6e8f9d5f9e0d8a1e7f4c1f3e12f60487233c
Pratik Kaje <kajepratik@gmail.com>
2024-10-22 21:34:40 +0400

    ShouldThrowValidationExceptionOnModifyIfTransactionIsNullAndLogItAsync => FAIL

---
+++ Tracker.Core.Api.Tests.Unit/Services/Foundations/Transactions/TransactionsServiceTests.Validations.Modify.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Tracker.Core.Api.Models.Foundations.Transactions;
+using Tracker.Core.Api.Models.Foundations.Transactions.Exceptions;
+
+namespace Tracker.Core.Api.Tests.Unit.Services.Foundations.Transactions
+{
+    public partial class TransactionsServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfTransactionIsNullAndLogItAsync()
+        {
+            // given
+            Transaction nullTransaction = null;
+
+            var nullTransactionException =
+                new NullTransactionException(
+                    message: "Transaction is null");
+
+            TransactionValidationException expectedTransactionValidationException =
+                new TransactionValidationException(
+                    message: "Transaction validation error occurred, fix the errors and try again.",
+                    innerException: nullTransactionException);
+
+            // when
+            ValueTask<Transaction> addTransactionTask =
+                this.transactionService.ModifyTransactionAsync(nullTransaction);
+
+            TransactionValidationException actualTransactionValidationException =
+                await Assert.ThrowsAsync<TransactionValidationException>(
+                    testCode: addTransactionTask.AsTask);
+
+            // then
+            actualTransactionValidationException.Should().BeEquivalentTo(
+                expectedTransactionValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(
+                    SameExceptionAs(expectedTransactionValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateTransactionAsync(It.IsAny<Transaction>()),
+                        Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.datetimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
