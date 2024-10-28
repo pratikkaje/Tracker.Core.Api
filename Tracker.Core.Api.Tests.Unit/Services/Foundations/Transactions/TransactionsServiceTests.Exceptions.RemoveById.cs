5b7a11f53b98bb6d69a5bdb615d6cf2894919993
Pratik Kaje <kajepratik@gmail.com>
2024-10-28 21:14:15 +0400

    ShouldThrowDependencyValidationExceptionOnRemoveByIdIfDbConcurrencyOccursAndLogItAsync => FAIL

--- Tracker.Core.Api.Tests.Unit/Services/Foundations/Transactions/TransactionsServiceTests.Exceptions.RemoveById.cs
+++ Tracker.Core.Api.Tests.Unit/Services/Foundations/Transactions/TransactionsServiceTests.Exceptions.RemoveById.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using Tracker.Core.Api.Models.Foundations.Transactions;
 using Tracker.Core.Api.Models.Foundations.Transactions.Exceptions;
@@ -58,6 +59,60 @@ namespace Tracker.Core.Api.Tests.Unit.Services.Foundations.Transactions
             this.datetimeBrokerMock.Verify(broker =>
                 broker.GetCurrentDateTimeOffsetAsync(),
                         Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.datetimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationExceptionOnRemoveByIdIfDbConcurrencyOccursAndLogItAsync()
+        {
+            // given
+            Guid someTransactionId = Guid.NewGuid();
+
+            var dbUpdateConcurrencyException =
+                new DbUpdateConcurrencyException();
+
+            var lockedTransactionException =
+                new LockedTransactionException(
+                    message: "Locked transaction record error occurred, please try again.",
+                    innerException: dbUpdateConcurrencyException,
+                    data: dbUpdateConcurrencyException.Data);
+
+            var expectedTransactionDependencyValidationException =
+                new TransactionDependencyValidationException(
+                    message: "Transaction dependency validation error occurred, fix errors and try again.",
+                    innerException: lockedTransactionException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectTransactionByIdAsync(someTransactionId))
+                    .ThrowsAsync(dbUpdateConcurrencyException);
+
+            // when
+            ValueTask<Transaction> removeTransactionByIdTask =
+                this.transactionService.RemoveTransactionByIdAsync(someTransactionId);
+
+            TransactionDependencyValidationException actualTransactionDependencyValidationException =
+                await Assert.ThrowsAsync<TransactionDependencyValidationException>(
+                    removeTransactionByIdTask.AsTask);
+
+            // then
+            actualTransactionDependencyValidationException.Should().BeEquivalentTo(
+                expectedTransactionDependencyValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectTransactionByIdAsync(someTransactionId),
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedTransactionDependencyValidationException))),
+                        Times.Once);
+
+            this.datetimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Never);
 
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
---
+++ Tracker.Core.Api/Models/Foundations/Transactions/Exceptions/LockedTransactionException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using Xeptions;
+
+namespace Tracker.Core.Api.Models.Foundations.Transactions.Exceptions
+{
+    public class LockedTransactionException : Xeption
+    {
+        public LockedTransactionException(string message, Exception innerException, IDictionary data)
+            : base(message, innerException, data)
+        { }
+    }
+}
