9ae089bc1494300d2c970b3856ee88ff5f9587d2
Pratik Kaje <kajepratik@gmail.com>
2024-10-30 21:54:55 +0400

    ShouldReturnInternalServerErrorOnPostIfServerErrorOccurredAsync => PASS

--- Tracker.Core.Api/Controllers/TransactionsController.cs
+++ Tracker.Core.Api/Controllers/TransactionsController.cs
@@ -40,6 +40,14 @@ namespace Tracker.Core.Api.Controllers
             {
                 return BadRequest(transactionDependencyValidationException.InnerException);
             }
+            catch (TransactionDependencyException transactionDependencyException)
+            {
+                return InternalServerError(transactionDependencyException);
+            }
+            catch (TransactionServiceException transactionServiceException)
+            {
+                return InternalServerError(transactionServiceException);
+            }
         }
     }
 }
