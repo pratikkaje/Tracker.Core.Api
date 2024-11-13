b619f6594404e69fbbf1dcf644de366d5b4c1359
Pratik Kaje <kajepratik@gmail.com>
2024-11-13 22:55:22 +0400

    ShouldThrowDependencyExceptionOnAddIfDependencyErrorOccurrsAndLogItAsync => FAIL

--- Tracker.Core.Api.Tests.Unit/Services/Foundations/Users/UsersServiceTests.Exceptions.Add.cs
+++ Tracker.Core.Api.Tests.Unit/Services/Foundations/Users/UsersServiceTests.Exceptions.Add.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using EFxceptions.Models.Exceptions;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using Tracker.Core.Api.Models.Foundations.Users;
 using Tracker.Core.Api.Models.Foundations.Users.Exceptions;
@@ -116,6 +117,59 @@ namespace Tracker.Core.Api.Tests.Unit.Services.Foundations.Users
             this.storageBrokerMock.Verify(broker =>
                 broker.InsertUserAsync(It.IsAny<User>()),
                     Times.Never);
+
+            this.datetimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnAddIfDependencyErrorOccurrsAndLogItAsync()
+        {
+            // given
+            DateTimeOffset someDate = GetRandomDateTimeOffset();
+            User someUser = CreateRandomUser(someDate);
+            var dbUpdateException = new DbUpdateException();
+
+            var failedOperationUserException =
+                new FailedOperationUserException(
+                    message: "Failed operation user error occurred, contact support.",
+                    innerException: dbUpdateException);
+
+            var expectedUserDependencyException =
+                new UserDependencyException(
+                    message: "User dependency error occurred, contact support.",
+                    innerException: failedOperationUserException);
+
+            this.datetimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ThrowsAsync(dbUpdateException);
+
+            // when
+            ValueTask<User> addUserTask =
+                this.userService.AddUserAsync(someUser);
+
+            UserDependencyException actualUserDependencyException =
+                 await Assert.ThrowsAsync<UserDependencyException>(
+                     testCode: addUserTask.AsTask);
+
+            // then
+            actualUserDependencyException.Should()
+                .BeEquivalentTo(expectedUserDependencyException);
+
+            this.datetimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedUserDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertUserAsync(
+                    It.IsAny<User>()),
+                        Times.Never);
 
             this.datetimeBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
---
+++ Tracker.Core.Api/Models/Foundations/Users/Exceptions/FailedOperationUserException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xeptions;
+
+namespace Tracker.Core.Api.Models.Foundations.Users.Exceptions
+{
+    public class FailedOperationUserException : Xeption
+    {
+        public FailedOperationUserException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
