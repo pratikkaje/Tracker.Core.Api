93d818a26d8450ebbfdb67d5b1ee5bc3bd0a0647
Pratik Kaje <kajepratik@gmail.com>
2024-11-17 16:32:44 +0400

    ShouldThrowValidationExceptionOnModifyIfUserIsNullAndLogItAsync => FAIL

---
+++ Tracker.Core.Api.Tests.Unit/Services/Foundations/Users/UsersServiceTests.Validations.Modify.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Tracker.Core.Api.Models.Foundations.Users;
+using Tracker.Core.Api.Models.Foundations.Users.Exceptions;
+
+namespace Tracker.Core.Api.Tests.Unit.Services.Foundations.Users
+{
+    public partial class UsersServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfUserIsNullAndLogItAsync()
+        {
+            // given
+            User nullUser = null;
+
+            var nullUserException =
+                new NullUserException(
+                    message: "User is null");
+
+            UserValidationException expectedUserValidationException =
+                new UserValidationException(
+                    message: "User validation error occurred, fix the errors and try again.",
+                    innerException: nullUserException);
+
+            // when
+            ValueTask<User> addUserTask =
+                this.userService.ModifyUserAsync(nullUser);
+
+            UserValidationException actualUserValidationException =
+                await Assert.ThrowsAsync<UserValidationException>(
+                    testCode: addUserTask.AsTask);
+
+            // then
+            actualUserValidationException.Should().BeEquivalentTo(
+                expectedUserValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(
+                    SameExceptionAs(expectedUserValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateUserAsync(It.IsAny<User>()),
+                        Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.datetimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
