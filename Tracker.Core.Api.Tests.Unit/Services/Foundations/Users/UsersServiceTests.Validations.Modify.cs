e35644a5a15a385682f2b0bae868e4ac2886aac2
Pratik Kaje <kajepratik@gmail.com>
2024-11-17 16:53:41 +0400

    ShouldThrowValidationExceptionOnModifyIfUserIsInvalidAndLogItAsync => FAIL

--- Tracker.Core.Api.Tests.Unit/Services/Foundations/Users/UsersServiceTests.Validations.Modify.cs
+++ Tracker.Core.Api.Tests.Unit/Services/Foundations/Users/UsersServiceTests.Validations.Modify.cs
@@ -52,5 +52,106 @@ namespace Tracker.Core.Api.Tests.Unit.Services.Foundations.Users
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.datetimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfUserIsInvalidAndLogItAsync(string invalidString)
+        {
+            // given
+            DateTimeOffset randomDateTimeOffset = default;
+
+            User invalidUser = new User
+            {
+                Id = Guid.Empty,
+                UserName = invalidString,
+                Name = invalidString,
+                Email = invalidString,
+                AvatarUrl = invalidString,
+                CreatedBy = invalidString,
+                ModifiedBy = invalidString,
+                CreatedDate = randomDateTimeOffset,
+                UpdatedDate = randomDateTimeOffset
+            };
+
+            var invalidUserException =
+                new InvalidUserException(
+                    message: "User is invalid, fix the errors and try again.");
+
+            invalidUserException.AddData(
+                key: nameof(User.Id),
+                values: "Id is invalid.");
+
+            invalidUserException.AddData(
+                key: nameof(User.UserName),
+                values: "Text is required.");
+
+            invalidUserException.AddData(
+                key: nameof(User.Name),
+                values: "Text is required.");
+
+            invalidUserException.AddData(
+                key: nameof(User.Email),
+                values: ["Text is required.", "Email not in valid format."]);
+
+            invalidUserException.AddData(
+                key: nameof(User.AvatarUrl),
+                values: "Text is required.");
+
+            invalidUserException.AddData(
+                key: nameof(User.CreatedBy),
+                values: "Text is required.");
+
+            invalidUserException.AddData(
+                key: nameof(User.ModifiedBy),
+                values: "Text is required.");
+
+            invalidUserException.AddData(
+                key: nameof(User.CreatedDate),
+                values: "Date is invalid.");
+
+            invalidUserException.AddData(
+                key: nameof(User.UpdatedDate),
+                values: ["Date is invalid.", $"Date is same as {nameof(User.CreatedDate)}"]);
+
+            var expectedUserValidationException =
+                new UserValidationException(
+                    message: "User validation error occurred, fix the errors and try again.",
+                    innerException: invalidUserException);
+
+            this.datetimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ReturnsAsync(randomDateTimeOffset);
+
+            // when
+            ValueTask<User> modifyUserTask =
+                this.userService.ModifyUserAsync(invalidUser);
+
+            UserValidationException actualUserValidationException =
+                await Assert.ThrowsAsync<UserValidationException>(
+                    testCode: modifyUserTask.AsTask);
+
+            // then
+            actualUserValidationException.Should()
+                .BeEquivalentTo(expectedUserValidationException);
+
+            this.datetimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(
+                    SameExceptionAs(expectedUserValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateUserAsync(It.IsAny<User>()),
+                    Times.Never);
+
+            this.datetimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
