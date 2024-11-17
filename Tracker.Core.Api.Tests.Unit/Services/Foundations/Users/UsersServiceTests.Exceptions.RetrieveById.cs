8476e4461f38d2427fb97dc94694ed4d2243e154
Pratik Kaje <kajepratik@gmail.com>
2024-11-17 13:49:04 +0400

    ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync => PASS

---
+++ Tracker.Core.Api.Tests.Unit/Services/Foundations/Users/UsersServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Tracker.Core.Api.Models.Foundations.Users;
+using Tracker.Core.Api.Models.Foundations.Users.Exceptions;
+
+namespace Tracker.Core.Api.Tests.Unit.Services.Foundations.Users
+{
+    public partial class UsersServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            var someUserId = Guid.NewGuid();
+            var sqlException = CreateSqlException();
+
+            var failedStorageUserException =
+                new FailedStorageUserException(
+                    message: "Failed user storage error occurred, contact support.",
+                    innerException: sqlException);
+
+            var expectedUserDependencyException =
+                new UserDependencyException(
+                    message: "User dependency error occurred, contact support.",
+                    innerException: failedStorageUserException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectUserByIdAsync(someUserId))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<User> retrieveUserByIdTask =
+                this.userService.RetrieveUserByIdAsync(someUserId);
+
+            UserDependencyException actualUserDependencyException =
+                await Assert.ThrowsAsync<UserDependencyException>(
+                    testCode: retrieveUserByIdTask.AsTask);
+
+            // then
+            actualUserDependencyException.Should().BeEquivalentTo(
+                expectedUserDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectUserByIdAsync(someUserId),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedUserDependencyException))),
+                        Times.Once);
+
+            this.datetimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.datetimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
