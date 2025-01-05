b80395716d250ab8a4bb46b6cd500a4227180717
Pratik Kaje <kajepratik@gmail.com>
2025-01-05 23:11:16 +0400

    ShouldThrowDependencyExceptionOnModifyIfDatabaseUpdateExceptionOccursAndLogItAsync => PASS

--- Tracker.Core.Api.Tests.Unit/Services/Foundations/Transactions/TransactionsServiceTests.Exceptions.Modify.cs
+++ Tracker.Core.Api.Tests.Unit/Services/Foundations/Transactions/TransactionsServiceTests.Exceptions.Modify.cs
@@ -55,11 +55,74 @@ namespace Tracker.Core.Api.Tests.Unit.Services.Foundations.Transactions
 						Times.Once);
 
 			this.storageBrokerMock.Verify(broker =>
-				broker.UpdateTransactionAsync(someUser),
+				broker.UpdateTransactionAsync(someTransaction),
 					Times.Never);
 
 			this.datetimeBrokerMock.VerifyNoOtherCalls();
 			this.storageBrokerMock.VerifyNoOtherCalls();
+			this.loggingBrokerMock.VerifyNoOtherCalls();
+		}
+
+		[Fact]
+		public async Task ShouldThrowDependencyExceptionOnModifyIfDatabaseUpdateExceptionOccursAndLogItAsync()
+		{
+			// given
+			int minutesInPast = CreateRandomNegativeNumber();
+			DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
+
+			Transaction randomTransaction =
+				CreateRandomTransaction(randomDateTimeOffset);
+
+			randomTransaction.CreatedDate =
+				randomDateTimeOffset.AddMinutes(minutesInPast);
+
+			var dbUpdateException = new DbUpdateException();
+
+			var failedOperationTransactionException =
+				new FailedOperationTransactionException(
+					message: "Failed operation transaction error occurred, contact support.",
+					innerException: dbUpdateException);
+
+			var expectedTransactionDependencyException =
+				new TransactionDependencyException(
+					message: "Transaction dependency error occurred, contact support.",
+					innerException: failedOperationTransactionException);
+
+			this.storageBrokerMock.Setup(broker =>
+				broker.SelectTransactionByIdAsync(randomTransaction.Id))
+					.ThrowsAsync(dbUpdateException);
+
+			this.datetimeBrokerMock.Setup(broker =>
+				broker.GetCurrentDateTimeOffsetAsync())
+					.ReturnsAsync(randomDateTimeOffset);
+
+			// when
+			ValueTask<Transaction> modifyTransactionTask =
+				this.transactionService.ModifyTransactionAsync(randomTransaction);
+
+			TransactionDependencyException actualTransactionDependencyException =
+				await Assert.ThrowsAsync<TransactionDependencyException>(
+					testCode: modifyTransactionTask.AsTask);
+
+			// then
+			actualTransactionDependencyException.Should().BeEquivalentTo(
+				expectedTransactionDependencyException);
+
+			this.storageBrokerMock.Verify(broker =>
+				broker.SelectTransactionByIdAsync(randomTransaction.Id),
+					Times.Once);
+
+			this.datetimeBrokerMock.Verify(broker =>
+				broker.GetCurrentDateTimeOffsetAsync(),
+					Times.Once);
+
+			this.loggingBrokerMock.Verify(broker =>
+				broker.LogErrorAsync(It.Is(SameExceptionAs(
+					expectedTransactionDependencyException))),
+						Times.Once);
+
+			this.storageBrokerMock.VerifyNoOtherCalls();
+			this.datetimeBrokerMock.VerifyNoOtherCalls();
 			this.loggingBrokerMock.VerifyNoOtherCalls();
 		}
 	}
