448363ad31d5d3330bc3bb339f9f543d2acac5cb
Pratik Kaje <kajepratik@gmail.com>
2025-01-08 21:00:51 +0400

    ShouldThrowCriticalDepedencyExceptionOnAddIfSqlErrorOccursAndLogItAsync => PASS

--- Tracker.Core.Api/Services/Foundations/Categories/CategoryService.Exceptions.cs
+++ Tracker.Core.Api/Services/Foundations/Categories/CategoryService.Exceptions.cs
@@ -3,6 +3,7 @@ using Tracker.Core.Api.Models.Foundations.Categories.Exceptions;
 using Tracker.Core.Api.Models.Foundations.Categories;
 using Xeptions;
 using Tracker.Core.Api.Models.Foundations.Transactions.Exceptions;
+using Microsoft.Data.SqlClient;
 
 namespace Tracker.Core.Api.Services.Foundations.Categories
 {
@@ -24,6 +25,27 @@ namespace Tracker.Core.Api.Services.Foundations.Categories
             {
                 throw await CreateAndLogValidationExceptionAsync(invalidCategoryException);
             }
+            catch (SqlException sqlException)
+            {
+                var failedStorageCategoryException =
+                    new FailedStorageCategoryException(
+                        message: "Category storage failed, contact support.",
+                        innerException: sqlException);
+
+                throw await CreateAndLogCriticalDependencyExceptionAsync(failedStorageCategoryException);
+            }
+        }
+
+        private async ValueTask<CategoryDependencyException> CreateAndLogCriticalDependencyExceptionAsync(Xeption exception)
+        {
+            var categoryDependencyException =
+                new CategoryDependencyException(
+                    message: "Category dependency error occurred, contact support.",
+                    innerException: exception);
+
+            await this.loggingBroker.LogCriticalAsync(categoryDependencyException);
+
+            return categoryDependencyException;
         }
 
         private async ValueTask<CategoryValidationException> CreateAndLogValidationExceptionAsync(Xeption exception)
