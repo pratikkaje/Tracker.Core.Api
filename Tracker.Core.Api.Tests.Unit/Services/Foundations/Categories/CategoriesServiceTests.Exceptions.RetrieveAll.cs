135992cfce232b416ab5a6502e51eae6fcaf15ad
Pratik Kaje <kajepratik@gmail.com>
2025-01-09 22:25:02 +0400

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSQLExceptionOccursAndLogItAsync => FAIL

---
+++ Tracker.Core.Api.Tests.Unit/Services/Foundations/Categories/CategoriesServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Tracker.Core.Api.Models.Foundations.Categories;
+using Tracker.Core.Api.Models.Foundations.Categories.Exceptions;
+
+namespace Tracker.Core.Api.Tests.Unit.Services.Foundations.Categories
+{
+    public partial class CategoriesServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSQLExceptionOccursAndLogItAsync()
+        {
+            // given
+            SqlException sqlException = CreateSqlException();
+
+            var failedStorageCategoryException =
+                new FailedStorageCategoryException(
+                    message: "Failed category storage error occurred, contact support.",
+                    innerException: sqlException);
+
+            var expectedCategoryDependencyException =
+                new CategoryDependencyException(
+                    message: "Category dependency error occurred, contact support.",
+                    innerException: failedStorageCategoryException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllCategoriesAsync())
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<IQueryable<Category>> retrieveAllCategorysTask =
+                this.categoryService.RetrieveAllCategoriesAsync();
+
+            CategoryDependencyException actualCategoryDependencyException =
+                await Assert.ThrowsAsync<CategoryDependencyException>(
+                    testCode: retrieveAllCategorysTask.AsTask);
+
+            // then
+            actualCategoryDependencyException.Should().BeEquivalentTo(
+                expectedCategoryDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllCategoriesAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedCategoryDependencyException))),
+                        Times.Once);
+
+            this.datetimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.datetimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
