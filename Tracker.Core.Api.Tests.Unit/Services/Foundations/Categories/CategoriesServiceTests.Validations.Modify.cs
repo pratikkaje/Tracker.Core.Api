fcbeeaf0f270a11c90e66c4009db24460efcc664
Pratik Kaje <kajepratik@gmail.com>
2025-01-12 21:59:39 +0400

    ShouldThrowValidationExceptionOnModifyIfCategoryIsNullAndLogItAsync=>PASS

---
+++ Tracker.Core.Api.Tests.Unit/Services/Foundations/Categories/CategoriesServiceTests.Validations.Modify.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Tracker.Core.Api.Models.Foundations.Categories;
+using Tracker.Core.Api.Models.Foundations.Categories.Exceptions;
+
+namespace Tracker.Core.Api.Tests.Unit.Services.Foundations.Categories
+{
+    public partial class CategoriesServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfCategoryIsNullAndLogItAsync()
+        {
+            // given
+            Category nullCategory = null;
+
+            var nullCategoryException =
+                new NullCategoryException(message: "Category is null.");
+
+            var expectedCategoryValidationException =
+                new CategoryValidationException(
+                    message: "Category validation error occurred, fix errors and try again.",
+                    innerException: nullCategoryException);
+
+            // when
+            ValueTask<Category> addCategoryTask =
+                this.categoryService.AddCategoryAsync(nullCategory);
+
+            CategoryValidationException actualCategoryValidationException =
+                await Assert.ThrowsAsync<CategoryValidationException>(
+                    testCode: addCategoryTask.AsTask);
+
+            // then
+            actualCategoryValidationException.Should().BeEquivalentTo(
+                expectedCategoryValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(
+                    SameExceptionAs(expectedCategoryValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertCategoryAsync(It.IsAny<Category>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.datetimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
