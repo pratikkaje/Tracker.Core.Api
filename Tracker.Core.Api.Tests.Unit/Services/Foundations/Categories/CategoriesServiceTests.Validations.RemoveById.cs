88813b219c4684bfafa671a297ff91317f7b74ec
Pratik Kaje <kajepratik@gmail.com>
2025-01-14 23:05:32 +0400

    ShouldThrowValidationExceptionOnRemoveByIdIfCategoryIdIsInvalidAndLogItAsync => FAIL

---
+++ Tracker.Core.Api.Tests.Unit/Services/Foundations/Categories/CategoriesServiceTests.Validations.RemoveById.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Tracker.Core.Api.Models.Foundations.Categories;
+using Tracker.Core.Api.Models.Foundations.Categories.Exceptions;
+using Tracker.Core.Api.Models.Foundations.Transactions.Exceptions;
+
+namespace Tracker.Core.Api.Tests.Unit.Services.Foundations.Categories
+{
+    public partial class CategoriesServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRemoveByIdIfCategoryIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid someCategoryId = Guid.Empty;
+
+            var invalidCategoryException =
+                new InvalidCategoryException(
+                    message: "Category is invalid, fix the errors and try again.");
+
+            invalidCategoryException.AddData(
+                key: nameof(Category.Id),
+                values: "Id is invalid.");
+
+            CategoryValidationException expectedCategoryValidationException =
+                new CategoryValidationException(
+                    message: "Category validation error occurred, fix errors and try again.",
+                    innerException: invalidCategoryException);
+
+            // when
+            ValueTask<Category> removeCategoryByIdTask =
+                this.categoryService.RemoveCategoryByIdAsync(someCategoryId);
+
+            CategoryValidationException actualCategoryValidationException =
+                await Assert.ThrowsAsync<CategoryValidationException>(
+                    removeCategoryByIdTask.AsTask);
+
+            // then
+            actualCategoryValidationException.Should().BeEquivalentTo(
+                expectedCategoryValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedCategoryValidationException))),
+                        Times.Once);
+
+            this.datetimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertCategoryAsync(It.IsAny<Category>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.datetimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
