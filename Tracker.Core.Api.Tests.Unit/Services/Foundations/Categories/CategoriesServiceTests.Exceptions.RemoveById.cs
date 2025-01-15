3980b4208ac57c6bbf090240c3f31f75ff8955f0
Pratik Kaje <kajepratik@gmail.com>
2025-01-15 22:36:17 +0400

    ShouldThrowDependencyValidationExceptionOnRemoveByIdIfDbConcurrencyOccursAndLogItAsync => PASS

--- Tracker.Core.Api.Tests.Unit/Services/Foundations/Categories/CategoriesServiceTests.Exceptions.RemoveById.cs
+++ Tracker.Core.Api.Tests.Unit/Services/Foundations/Categories/CategoriesServiceTests.Exceptions.RemoveById.cs
@@ -5,10 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using Tracker.Core.Api.Models.Foundations.Categories;
 using Tracker.Core.Api.Models.Foundations.Categories.Exceptions;
-using Tracker.Core.Api.Models.Foundations.Transactions.Exceptions;
 
 namespace Tracker.Core.Api.Tests.Unit.Services.Foundations.Categories
 {
@@ -59,6 +59,61 @@ namespace Tracker.Core.Api.Tests.Unit.Services.Foundations.Categories
             this.datetimeBrokerMock.Verify(broker =>
                 broker.GetCurrentDateTimeOffsetAsync(),
                         Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.datetimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationExceptionOnRemoveByIdIfDbConcurrencyOccursAndLogItAsync()
+        {
+            // given
+            Guid someCategoryId = Guid.NewGuid();
+
+            var dbUpdateConcurrencyException =
+                new DbUpdateConcurrencyException();
+
+            var lockedCategoryException =
+                new LockedCategoryException(
+                    message: "Locked category record error occurred, please try again.",
+                    innerException: dbUpdateConcurrencyException,
+                    data: dbUpdateConcurrencyException.Data);
+
+            var expectedCategoryDependencyValidationException =
+                new CategoryDependencyValidationException(
+                    message: "Category dependency validation error occurred, fix errors and try again.",
+                    innerException: lockedCategoryException,
+                    data: lockedCategoryException.Data);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectCategoryByIdAsync(someCategoryId))
+                    .ThrowsAsync(dbUpdateConcurrencyException);
+
+            // when
+            ValueTask<Category> removeCategoryByIdTask =
+                this.categoryService.RemoveCategoryByIdAsync(someCategoryId);
+
+            CategoryDependencyValidationException actualCategoryDependencyValidationException =
+                await Assert.ThrowsAsync<CategoryDependencyValidationException>(
+                    removeCategoryByIdTask.AsTask);
+
+            // then
+            actualCategoryDependencyValidationException.Should().BeEquivalentTo(
+                expectedCategoryDependencyValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectCategoryByIdAsync(someCategoryId),
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedCategoryDependencyValidationException))),
+                        Times.Once);
+
+            this.datetimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Never);
 
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
