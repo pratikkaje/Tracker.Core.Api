5cd8c2b01ca70bdfcd9d29cf6e938a3c32dfe19b
Pratik Kaje <kajepratik@gmail.com>
2025-01-16 21:50:39 +0400

    CODE RUB: Added Created By and Updated By Fields to Category Model

--- Tracker.Core.Api/Migrations/20250116174935_AddAuditFieldsToCategoryModel.Designer.cs
+++ Tracker.Core.Api/Migrations/20250116174935_AddAuditFieldsToCategoryModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Tracker.Core.Api.Brokers.Storages;
 
@@ -11,9 +12,11 @@ using Tracker.Core.Api.Brokers.Storages;
 namespace Tracker.Core.Api.Migrations
 {
     [DbContext(typeof(StorageBroker))]
-    partial class StorageBrokerModelSnapshot : ModelSnapshot
+    [Migration("20250116174935_AddAuditFieldsToCategoryModel")]
+    partial class AddAuditFieldsToCategoryModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
