4be4c9ba8efc3abb131429dd0ee3dd859be841e9
Pratik Kaje <kajepratik@gmail.com>
2025-01-20 22:26:06 +0400

    ShouldReturnInternalServerErrorOnGetByIdIfServerErrorOccursAsync => FAIL

--- Tracker.Core.Api.Tests.Unit/Controllers/Transactions/TransactionsControllerTests.Exception.GetById.cs
+++ Tracker.Core.Api.Tests.Unit/Controllers/Transactions/TransactionsControllerTests.Exception.GetById.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using RESTFulSense.Clients.Extensions;
+using RESTFulSense.Models;
 using Tracker.Core.Api.Models.Foundations.Transactions;
 using Xeptions;
 
@@ -22,6 +23,38 @@ namespace Tracker.Core.Api.Tests.Unit.Controllers.Transactions
 
             BadRequestObjectResult expectedBadRequestObjectResult =
                 BadRequest(validationException.InnerException);
+
+            var expectedActionResult =
+                new ActionResult<Transaction>(expectedBadRequestObjectResult);
+
+            this.transactionServiceMock.Setup(service =>
+                service.RetrieveTransactionByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(validationException);
+
+            // when
+            ActionResult<Transaction> actualActionResult =
+                await this.transactionsController.GetTransactionByIdAsync(someId);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.transactionServiceMock.Verify(service =>
+                service.RetrieveTransactionByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.transactionServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(ServerExceptions))]
+        public async Task ShouldReturnInternalServerErrorOnGetByIdIfServerErrorOccursAsync(
+            Xeption validationException)
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+
+            InternalServerErrorObjectResult expectedBadRequestObjectResult =
+                InternalServerError(validationException);
 
             var expectedActionResult =
                 new ActionResult<Transaction>(expectedBadRequestObjectResult);
