fa1ad4692348d997ffa0cbf2ba300295f5b26867
Pratik Kaje <kajepratik@gmail.com>
2025-01-20 22:27:49 +0400

    ShouldReturnInternalServerErrorOnGetByIdIfServerErrorOccursAsync => PASS

--- Tracker.Core.Api/Controllers/TransactionsController.cs
+++ Tracker.Core.Api/Controllers/TransactionsController.cs
@@ -94,6 +94,14 @@ namespace Tracker.Core.Api.Controllers
             {
                 return BadRequest(transactionDependencyValidationException.InnerException);
             }
+            catch (TransactionDependencyException transactionDependencyException)
+            {
+                return InternalServerError(transactionDependencyException);
+            }
+            catch (TransactionServiceException transactionServiceException)
+            {
+                return InternalServerError(transactionServiceException);
+            }
 
         }
 
