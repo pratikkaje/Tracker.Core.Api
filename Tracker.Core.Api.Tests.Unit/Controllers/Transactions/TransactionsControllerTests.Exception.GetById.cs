4d82b6db0538229fd8c05cad3ae81bc42da88e24
Pratik Kaje <kajepratik@gmail.com>
2025-01-20 21:25:26 +0400

    ShouldReturnBadRequestOnGetByIdIfValidationErrorOccursAsync => FAIL

---
+++ Tracker.Core.Api.Tests.Unit/Controllers/Transactions/TransactionsControllerTests.Exception.GetById.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RESTFulSense.Clients.Extensions;
+using Tracker.Core.Api.Models.Foundations.Transactions;
+using Xeptions;
+
+namespace Tracker.Core.Api.Tests.Unit.Controllers.Transactions
+{
+    public partial class TransactionsControllerTests
+    {
+        [Theory]
+        [MemberData(nameof(ValidationExceptions))]
+        public async Task ShouldReturnBadRequestOnGetByIdIfValidationErrorOccursAsync(Xeption validationException)
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+
+            BadRequestObjectResult expectedBadRequestObjectResult =
+                BadRequest(validationException.InnerException);
+
+            var expectedActionResult =
+                new ActionResult<Transaction>(expectedBadRequestObjectResult);
+
+            this.transactionServiceMock.Setup(service =>
+                service.RetrieveTransactionByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(validationException);
+
+            // when
+            ActionResult<Transaction> actualActionResult =
+                await this.transactionsController.GetTransactionByIdAsync(someId);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.transactionServiceMock.Verify(service =>
+                service.RetrieveTransactionByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.transactionServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
