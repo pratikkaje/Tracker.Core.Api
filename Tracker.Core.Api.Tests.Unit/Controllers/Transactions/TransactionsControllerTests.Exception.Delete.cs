af5ad861d9399cd73ca3b9de758066926807b779
Pratik Kaje <kajepratik@gmail.com>
2025-01-24 22:41:46 +0400

    ShouldReturnNotFoundOnDeleteIfItemDoesNotExistAsync => PASS

--- Tracker.Core.Api.Tests.Unit/Controllers/Transactions/TransactionsControllerTests.Exception.Delete.cs
+++ Tracker.Core.Api.Tests.Unit/Controllers/Transactions/TransactionsControllerTests.Exception.Delete.cs
@@ -8,6 +8,7 @@ using Moq;
 using RESTFulSense.Clients.Extensions;
 using RESTFulSense.Models;
 using Tracker.Core.Api.Models.Foundations.Transactions;
+using Tracker.Core.Api.Models.Foundations.Transactions.Exceptions;
 using Xeptions;
 
 namespace Tracker.Core.Api.Tests.Unit.Controllers.Transactions
@@ -62,6 +63,46 @@ namespace Tracker.Core.Api.Tests.Unit.Controllers.Transactions
             this.transactionServiceMock.Setup(service =>
                 service.RemoveTransactionByIdAsync(It.IsAny<Guid>()))
                     .ThrowsAsync(validationException);
+
+            // when
+            ActionResult<Transaction> actualActionResult =
+                await this.transactionsController.DeleteTransactionByIdAsync(someId);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.transactionServiceMock.Verify(service =>
+                service.RemoveTransactionByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.transactionServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldReturnNotFoundOnDeleteIfItemDoesNotExistAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            string someMessage = GetRandomString();
+
+            var notFoundTransactionException =
+                new NotFoundTransactionException(
+                    message: someMessage);
+
+            var transactionValidationException =
+                new TransactionValidationException(
+                    message: someMessage,
+                    innerException: notFoundTransactionException);
+
+            NotFoundObjectResult expectedNotFoundObjectResult =
+                NotFound(notFoundTransactionException);
+
+            var expectedActionResult =
+                new ActionResult<Transaction>(expectedNotFoundObjectResult);
+
+            this.transactionServiceMock.Setup(service =>
+                service.RemoveTransactionByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(transactionValidationException);
 
             // when
             ActionResult<Transaction> actualActionResult =
