66546294b0751ed936b20029e59aa30d72d722a0
Pratik Kaje <kajepratik@gmail.com>
2025-01-31 22:52:55 +0400

    ShouldReturnBadRequestOnGetByIdIfValidationErrorOccursAsync => FAIL

---
+++ Tracker.Core.Api.Tests.Unit/Controllers/Users/UsersControllerTests.Exception.GetById.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RESTFulSense.Clients.Extensions;
+using Tracker.Core.Api.Models.Foundations.Users;
+using Xeptions;
+
+namespace Tracker.Core.Api.Tests.Unit.Controllers.Users
+{
+    public partial class UsersControllerTests
+    {
+        [Theory]
+        [MemberData(nameof(ValidationExceptions))]
+        public async Task ShouldReturnBadRequestOnGetByIdIfValidationErrorOccursAsync(Xeption validationException)
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+
+            BadRequestObjectResult expectedBadRequestObjectResult =
+                BadRequest(validationException.InnerException);
+
+            var expectedActionResult =
+                new ActionResult<User>(expectedBadRequestObjectResult);
+
+            this.userServiceMock.Setup(service =>
+                service.RetrieveUserByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(validationException);
+
+            // when
+            ActionResult<User> actualActionResult =
+                await this.usersController.GetUserByIdAsync(someId);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.userServiceMock.Verify(service =>
+                service.RetrieveUserByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.userServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
