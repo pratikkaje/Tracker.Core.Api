68241a49fca1aa2373fbe53910616e220baea9cc
Pratik Kaje <kajepratik@gmail.com>
2025-02-02 20:40:36 +0400

    ShouldReturnNotFoundOnPutIfItemDoesNotExistAsync => PASS

--- Tracker.Core.Api.Tests.Unit/Controllers/Users/UsersControllerTests.Exception.Put.cs
+++ Tracker.Core.Api.Tests.Unit/Controllers/Users/UsersControllerTests.Exception.Put.cs
@@ -7,7 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 using Moq;
 using RESTFulSense.Clients.Extensions;
 using RESTFulSense.Models;
+using Tracker.Core.Api.Models.Foundations.Transactions.Exceptions;
 using Tracker.Core.Api.Models.Foundations.Users;
+using Tracker.Core.Api.Models.Foundations.Users.Exceptions;
 using Xeptions;
 
 namespace Tracker.Core.Api.Tests.Unit.Controllers.Users
@@ -62,6 +64,46 @@ namespace Tracker.Core.Api.Tests.Unit.Controllers.Users
             this.userServiceMock.Setup(service =>
                 service.ModifyUserAsync(It.IsAny<User>()))
                     .ThrowsAsync(validationException);
+
+            // when
+            ActionResult<User> actualActionResult =
+                await this.usersController.PutUserAsync(someUser);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.userServiceMock.Verify(service =>
+                service.ModifyUserAsync(It.IsAny<User>()),
+                    Times.Once);
+
+            this.userServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldReturnNotFoundOnPutIfItemDoesNotExistAsync()
+        {
+            // given
+            User someUser = CreateRandomUser();
+            string someMessage = GetRandomString();
+
+            var notFoundUserException =
+                new NotFoundUserException(
+                    message: someMessage);
+
+            var userValidationException =
+                new UserValidationException(
+                    message: someMessage,
+                    innerException: notFoundUserException);
+
+            NotFoundObjectResult expectedNotFoundObjectResult =
+                NotFound(notFoundUserException);
+
+            var expectedActionResult =
+                new ActionResult<User>(expectedNotFoundObjectResult);
+
+            this.userServiceMock.Setup(service =>
+                service.ModifyUserAsync(It.IsAny<User>()))
+                    .ThrowsAsync(userValidationException);
 
             // when
             ActionResult<User> actualActionResult =
