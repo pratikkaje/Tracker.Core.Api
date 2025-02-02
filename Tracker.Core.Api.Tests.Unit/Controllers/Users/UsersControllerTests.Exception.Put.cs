746ce19d0ef476eb4034e743af47d2de3c2fd671
Pratik Kaje <kajepratik@gmail.com>
2025-02-02 20:44:34 +0400

    ShouldReturnConflictOnPutIfAlreadyExistsUserErrorOccursAsync => PASS

--- Tracker.Core.Api.Tests.Unit/Controllers/Users/UsersControllerTests.Exception.Put.cs
+++ Tracker.Core.Api.Tests.Unit/Controllers/Users/UsersControllerTests.Exception.Put.cs
@@ -7,9 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 using Moq;
 using RESTFulSense.Clients.Extensions;
 using RESTFulSense.Models;
-using Tracker.Core.Api.Models.Foundations.Transactions.Exceptions;
-using Tracker.Core.Api.Models.Foundations.Users;
 using Tracker.Core.Api.Models.Foundations.Users.Exceptions;
+using Tracker.Core.Api.Models.Foundations.Users;
 using Xeptions;
 
 namespace Tracker.Core.Api.Tests.Unit.Controllers.Users
@@ -118,5 +117,51 @@ namespace Tracker.Core.Api.Tests.Unit.Controllers.Users
 
             this.userServiceMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldReturnConflictOnPutIfAlreadyExistsUserErrorOccursAsync()
+        {
+            // given
+            User someUser = CreateRandomUser();
+            var someInnerException = new Exception();
+            string someMessage = GetRandomString();
+
+            var alreadyExistsUserException =
+                new AlreadyExistsUserException(
+                    message: someMessage,
+                    innerException: someInnerException,
+                    data: someInnerException.Data);
+
+            var userDependencyValidationException =
+                new UserDependencyValidationException(
+                    message: someMessage,
+                    innerException: alreadyExistsUserException,
+                    data: alreadyExistsUserException.Data);
+                
+
+            ConflictObjectResult expectedConflictObjectResult =
+                Conflict(alreadyExistsUserException);
+
+            var expectedActionResult =
+                new ActionResult<User>(expectedConflictObjectResult);
+
+            this.userServiceMock.Setup(service =>
+                service.ModifyUserAsync(It.IsAny<User>()))
+                    .ThrowsAsync(userDependencyValidationException);
+
+            // when
+            ActionResult<User> actualActionResult =
+                await this.usersController.PutUserAsync(someUser);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.userServiceMock.Verify(service =>
+                service.ModifyUserAsync(It.IsAny<User>()),
+                    Times.Once);
+
+            this.userServiceMock.VerifyNoOtherCalls();
+        }
+
     }
 }
