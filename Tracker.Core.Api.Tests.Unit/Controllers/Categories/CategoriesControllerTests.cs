6dc5664a341a8a6434b7e6e479cb6fe253b75eba
Pratik Kaje <kajepratik@gmail.com>
2025-02-05 18:56:15 +0400

    ShouldReturnCreatedOnPostAsync => FAIL

---
+++ Tracker.Core.Api.Tests.Unit/Controllers/Categories/CategoriesControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Tracker.Core.Api.Controllers;
+using Tracker.Core.Api.Models.Foundations.Categories;
+using Tracker.Core.Api.Services.Foundations.Categories;
+using Tracker.Core.Api.Services.Foundations.Transactions;
+using Tynamix.ObjectFiller;
+
+namespace Tracker.Core.Api.Tests.Unit.Controllers.Categories
+{
+    public partial class CategoriesControllerTests
+    {
+        private readonly Mock<ICategoryService> categoryServiceMock;
+        private readonly CategoriesController categoriesController;
+        public CategoriesControllerTests()
+        {
+            this.categoryServiceMock = new Mock<ICategoryService>();
+
+            this.categoriesController =
+                new CategoriesController(
+                    categoryService: this.categoryServiceMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static Category CreateRandomCategory() =>
+            CreateCategoryFiller().Create();
+
+        private static Filler<Category> CreateCategoryFiller()
+        {
+            var filler = new Filler<Category>();
+            string user = Guid.NewGuid().ToString();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(GetRandomDateTimeOffset())
+                .OnProperty(category => category.CreatedBy).Use(user)
+                .OnProperty(category => category.UpdatedBy).Use(user);
+
+            return filler;
+        }
+
+    }
+}
---
+++ Tracker.Core.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using Tracker.Core.Api.Models.Foundations.Categories;
+using Tracker.Core.Api.Services.Foundations.Categories;
+
+namespace Tracker.Core.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriesController : RESTFulController
+    {
+        private readonly ICategoryService categoryService;
+
+        public CategoriesController(ICategoryService categoryService) =>
+            this.categoryService = categoryService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<Category>> PostCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
