ee3c52a30909223dd790e109d56b2e3e9e942b5d
Pratik Kaje <kajepratik@gmail.com>
2025-02-06 16:45:48 +0400

    ShouldReturnInternalServerErrorOnPostIfServerErrorOccurredAsync => PASS

--- Tracker.Core.Api/Controllers/CategoriesController.cs
+++ Tracker.Core.Api/Controllers/CategoriesController.cs
@@ -42,6 +42,14 @@ namespace Tracker.Core.Api.Controllers
             {
                 return BadRequest(categoryDependencyValidationException.InnerException);
             }
+            catch (CategoryDependencyException categoryDependencyException)
+            {
+                return InternalServerError(categoryDependencyException);
+            }
+            catch (CategoryServiceException categoryServiceException)
+            {
+                return InternalServerError(categoryServiceException);
+            }
         }
     }
 }
