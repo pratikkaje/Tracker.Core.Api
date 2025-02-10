e752568f1cdd9061ec635f0052a89745d0cb2401
Pratik Kaje <kajepratik@gmail.com>
2025-02-10 22:43:55 +0400

    ShouldReturnBadRequestOnGetByIdIfValidationErrorOccursAsync => FAIL

---
+++ Tracker.Core.Api.Tests.Unit/Controllers/Categories/CategoriesControllerTests.Exception.GetById.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RESTFulSense.Clients.Extensions;
+using RESTFulSense.Models;
+using Tracker.Core.Api.Models.Foundations.Categories;
+using Tracker.Core.Api.Models.Foundations.Categories.Exceptions;
+using Xeptions;
+
+namespace Tracker.Core.Api.Tests.Unit.Controllers.Categories
+{
+    public partial class CategoriesControllerTests
+    {
+        [Theory]
+        [MemberData(nameof(ValidationExceptions))]
+        public async Task ShouldReturnBadRequestOnGetByIdIfValidationErrorOccursAsync(Xeption validationException)
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+
+            BadRequestObjectResult expectedBadRequestObjectResult =
+                BadRequest(validationException.InnerException);
+
+            var expectedActionResult =
+                new ActionResult<Category>(expectedBadRequestObjectResult);
+
+            this.categoryServiceMock.Setup(service =>
+                service.RetrieveCategoryByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(validationException);
+
+            // when
+            ActionResult<Category> actualActionResult =
+                await this.categoriesController.GetCategoryByIdAsync(someId);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.categoryServiceMock.Verify(service =>
+                service.RetrieveCategoryByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.categoryServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
