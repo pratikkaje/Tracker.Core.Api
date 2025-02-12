b89db2b71076d972bb41d267b8202c72873fa41d
Pratik Kaje <kajepratik@gmail.com>
2025-02-12 23:48:48 +0400

    ShouldReturnBadRequestOnPutIfValidationErrorOccursAsync => FAIL

---
+++ Tracker.Core.Api.Tests.Unit/Controllers/Categories/CategoriesControllerTests.Exception.Put.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RESTFulSense.Clients.Extensions;
+using Tracker.Core.Api.Models.Foundations.Categories;
+using Tracker.Core.Api.Models.Foundations.Users;
+using Xeptions;
+
+namespace Tracker.Core.Api.Tests.Unit.Controllers.Categories
+{
+    public partial class CategoriesControllerTests
+    {
+        [Theory]
+        [MemberData(nameof(ValidationExceptions))]
+        public async Task ShouldReturnBadRequestOnPutIfValidationErrorOccursAsync(Xeption validationException)
+        {
+            // given
+            Category someCategory = CreateRandomCategory();
+
+            BadRequestObjectResult expectedBadRequestObjectResult =
+                BadRequest(validationException.InnerException);
+
+            var expectedActionResult =
+                new ActionResult<Category>(expectedBadRequestObjectResult);
+
+            this.categoryServiceMock.Setup(service =>
+                service.ModifyCategoryAsync(It.IsAny<Category>()))
+                    .ThrowsAsync(validationException);
+
+            // when
+            ActionResult<Category> actualActionResult =
+                await this.categoriesController.PutCategoryAsync(someCategory);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.categoryServiceMock.Verify(service =>
+                service.ModifyCategoryAsync(It.IsAny<Category>()),
+                    Times.Once);
+
+            this.categoryServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
