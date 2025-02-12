982271964f2a7abce2a1e903e7c5862f20988511
Pratik Kaje <kajepratik@gmail.com>
2025-02-12 23:53:28 +0400

    ShouldReturnInternalServerErrorOnPutIfServerErrorOccurredAsync => FAIL

--- Tracker.Core.Api.Tests.Unit/Controllers/Categories/CategoriesControllerTests.Exception.Put.cs
+++ Tracker.Core.Api.Tests.Unit/Controllers/Categories/CategoriesControllerTests.Exception.Put.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using RESTFulSense.Clients.Extensions;
+using RESTFulSense.Models;
 using Tracker.Core.Api.Models.Foundations.Categories;
 using Tracker.Core.Api.Models.Foundations.Users;
 using Xeptions;
@@ -23,6 +24,38 @@ namespace Tracker.Core.Api.Tests.Unit.Controllers.Categories
 
             BadRequestObjectResult expectedBadRequestObjectResult =
                 BadRequest(validationException.InnerException);
+
+            var expectedActionResult =
+                new ActionResult<Category>(expectedBadRequestObjectResult);
+
+            this.categoryServiceMock.Setup(service =>
+                service.ModifyCategoryAsync(It.IsAny<Category>()))
+                    .ThrowsAsync(validationException);
+
+            // when
+            ActionResult<Category> actualActionResult =
+                await this.categoriesController.PutCategoryAsync(someCategory);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.categoryServiceMock.Verify(service =>
+                service.ModifyCategoryAsync(It.IsAny<Category>()),
+                    Times.Once);
+
+            this.categoryServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(ServerExceptions))]
+        public async Task ShouldReturnInternalServerErrorOnPutIfServerErrorOccurredAsync(
+            Xeption validationException)
+        {
+            // given
+            Category someCategory = CreateRandomCategory();
+
+            InternalServerErrorObjectResult expectedBadRequestObjectResult =
+                InternalServerError(validationException);
 
             var expectedActionResult =
                 new ActionResult<Category>(expectedBadRequestObjectResult);
